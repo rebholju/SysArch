6c2b3ee105b7f58ef7bda23c34e7c343a9d013bd
Julian_Rebholz <Julian_Rebholz@DESKTOP-TH1PH4D>
2019-05-02 11:17:43 +0200

    second

--- webserverSysArch/index.php
+++ webserverSysArch/index.php
@@ -2,19 +2,38 @@
 <html>
 <head>
 	<title>Login</title>
+	<link href="stylesheet.css" rel="stylesheet">
 </head>
 <body>
 <h1>Login</h1>
-<form action="login.php" method="post">
+<div id="login">
+<?php 
+if(isset($_POST['submit']))
+    {
+         
+        if( $_POST["uid"]== "test" and $_POST["pwd"] == "1234"){
+            echo'<div id=loginr> successfully login </div>';
+        }
+        else{
+            echo'<div id=loginf>wrong password</div>';
+        }
+            
+    }
+    
+
+?>
+<form action="?login=1" method="post">
 	<input type="text" name="uid" placeholder="username/e-mail"><br />
 	<input type="password" name="pwd" placeholder="password"><br />
 	<button type="submit" name="submit" >Login</button>
 </form>
 	<a href="signup.php">Sign up</a>
 
+</div>
 </body>
 
 
 
 
 </html>
+
--- webserverSysArch/signup.php
+++ webserverSysArch/signup.php
@@ -2,9 +2,11 @@
 <html>
 <head>
 	<title>Sign up</title>
+	<link href="stylesheet.css" rel="stylesheet">
 </head>
 <body>
 <h1>Sign up</h1>
+<div id="signup">
 <form action="intodatastorage.php" method="post">
 	<input type="text" name="firstname" placeholder="Firstname"><br />
 	<input type="text" name="secondname" placeholder="Secondname"><br />
@@ -14,7 +16,7 @@
 	<button type="submit" name="submit" >Sign in</button>
 </form>
 	<a href="index.php">Home</a>
-
+</div>
 </body>
 
 
