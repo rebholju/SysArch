37282b8ff101bc69e5266f6f6e6fa456b022cb27
Julian_Rebholz <Julian_Rebholz@DESKTOP-1AU8LDO>
2019-06-12 22:52:06 +0200

    Datenbank

--- webserverSysArch/SQL_Datenbank/sysarch.sql
+++ webserverSysArch/SQL_Datenbank/sysarch.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Jun 2019 um 17:35
+-- Erstellungszeit: 12. Jun 2019 um 22:48
 -- Server-Version: 10.1.40-MariaDB
 -- PHP-Version: 7.3.5
 
@@ -46,8 +46,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`idUsers`, `role`, `username`, `firstname`, `lastname`, `email`, `password`, `lastlogin`, `resetpassword`, `rfidID`) VALUES
-(14, 10, 'rebholju', 'Julian', 'Rebholz', 'rebholju@googlemail.com', '$2y$10$GLP7r3wRuh0VRRa7Ssfsfehr0/.qmHMGGbiYy3xZAVWShI7G1SLOK', '2019-06-11 17:24:35', '', 12345),
-(15, 20, 'ju721reb', 'Julian', 'Rebholz', 'rebholju@googlemail.com', '$2y$10$d/l5q2XIHP1CsMkDxdslJOpPiTUZg262xavYqME1G9sR.UYM75oqW', '2019-06-11 17:13:36', '', 0),
+(14, 10, 'rebholju', 'Julian', 'Rebholz', 'rebholju@googlemail.com', '$2y$10$GLP7r3wRuh0VRRa7Ssfsfehr0/.qmHMGGbiYy3xZAVWShI7G1SLOK', '2019-06-12 22:47:18', '', 12345),
+(15, 20, 'ju721reb', 'Julian', 'Rebholz', 'rebholju@googlemail.com', '$2y$10$d/l5q2XIHP1CsMkDxdslJOpPiTUZg262xavYqME1G9sR.UYM75oqW', '2019-06-12 22:47:36', '', 123),
 (20, 20, 'HalloTest', 'asaf', 'asfasf', '1123@web.de', '$2y$10$bvgxnf5bX3ijVJbQBOStTOv1WfVqyVgLsab50O0mfIqmf01OCzkMG', '0000-00-00 00:00:00', '', 2142),
 (21, 20, 'test', 'test1', 'ttest', 'asf@gmail.com', '$2y$10$vegMpfIz4Un00vGx5YGBUuyR2hBLEOnggycZx7BpLtKNyyqIe9.mm', '2019-06-11 17:24:46', '', 213);
 
@@ -60,9 +60,9 @@ INSERT INTO `users` (`idUsers`, `role`, `username`, `firstname`, `lastname`, `em
 CREATE TABLE `vehiclecurrentdata` (
   `vehicleNumber` int(11) NOT NULL,
   `sensor` tinytext NOT NULL,
-  `value` varchar(20) NOT NULL,
-  `timeStamp` datetime NOT NULL,
-  `driver` tinytext NOT NULL
+  `value` varchar(20) DEFAULT NULL,
+  `timeStamp` datetime DEFAULT NULL,
+  `driver` tinytext
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -70,12 +70,18 @@ CREATE TABLE `vehiclecurrentdata` (
 --
 
 INSERT INTO `vehiclecurrentdata` (`vehicleNumber`, `sensor`, `value`, `timeStamp`, `driver`) VALUES
-(1, 'CPUTemp', '12', '2019-06-08 17:00:12', 'rebholju'),
-(1, 'jitter', '8', '2019-06-08 17:00:12', 'rebholju'),
-(1, 'numOfRTThreads', 'eins', '2019-06-08 17:00:12', 'rebholju'),
-(1, 'LIDAR', '190', '2019-06-08 17:00:12', 'rebholju'),
-(1, 'Speed', '22', '2019-06-08 17:00:12', 'rebholju'),
-(1, 'BatteryPower', '11', '2019-06-08 17:00:12', 'rebholju');
+(1, 'CPUTemp', '300', '2019-06-12 18:05:48', 'rebholju'),
+(1, 'jitter', '8', '2019-06-12 18:05:48', 'rebholju'),
+(1, 'numOfRTThreads', 'test', '2019-06-12 18:05:48', 'rebholju'),
+(1, 'LIDAR', '190', '2019-06-12 18:05:48', 'rebholju'),
+(1, 'Speed', '22', '2019-06-12 18:05:48', 'rebholju'),
+(1, 'BatteryPower', '11', '2019-06-12 18:05:48', 'rebholju'),
+(2, 'CPUTemp', '300', '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'jitter', '8', '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'numOfRTThreads', 'test', '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'LIDAR', '190', '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'Speed', '22', '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'BatteryPower', '11', '2019-06-12 18:02:52', 'ju721reb');
 
 -- --------------------------------------------------------
 
@@ -96,18 +102,48 @@ CREATE TABLE `vehiclehistoricaldata` (
 --
 
 INSERT INTO `vehiclehistoricaldata` (`vehicleNumber`, `sensor`, `value`, `timeStamp`, `driver`) VALUES
-(1, 'CPUTemp', 12, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'jitter', 8, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'numOfRTThreads', 0, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'LIDAR', 190, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'Speed', 22, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'BatteryPower', 11, '2019-06-08 17:00:09', 'rebholju'),
-(1, 'CPUTemp', 12, '2019-06-08 17:00:12', 'rebholju'),
-(1, 'jitter', 8, '2019-06-08 17:00:12', 'rebholju'),
-(1, 'numOfRTThreads', 0, '2019-06-08 17:00:12', 'rebholju'),
-(1, 'LIDAR', 190, '2019-06-08 17:00:12', 'rebholju'),
-(1, 'Speed', 22, '2019-06-08 17:00:12', 'rebholju'),
-(1, 'BatteryPower', 11, '2019-06-08 17:00:12', 'rebholju');
+(2, 'CPUTemp', 300, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'jitter', 8, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'numOfRTThreads', 0, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'LIDAR', 190, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'Speed', 22, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'BatteryPower', 11, '2019-06-12 17:59:53', 'rebholju'),
+(2, 'CPUTemp', 300, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'jitter', 8, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'numOfRTThreads', 0, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'LIDAR', 190, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'Speed', 22, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'BatteryPower', 11, '2019-06-12 18:00:33', 'rebholju'),
+(2, 'CPUTemp', 300, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'jitter', 8, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'numOfRTThreads', 0, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'LIDAR', 190, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'Speed', 22, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'BatteryPower', 11, '2019-06-12 18:01:37', 'rebholju'),
+(2, 'CPUTemp', 300, '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'jitter', 8, '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'numOfRTThreads', 0, '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'LIDAR', 190, '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'Speed', 22, '2019-06-12 18:02:52', 'ju721reb'),
+(2, 'BatteryPower', 11, '2019-06-12 18:02:52', 'ju721reb'),
+(1, 'CPUTemp', 300, '2019-06-12 18:03:55', 'ju721reb'),
+(1, 'jitter', 8, '2019-06-12 18:03:55', 'ju721reb'),
+(1, 'numOfRTThreads', 0, '2019-06-12 18:03:55', 'ju721reb'),
+(1, 'LIDAR', 190, '2019-06-12 18:03:56', 'ju721reb'),
+(1, 'Speed', 22, '2019-06-12 18:03:56', 'ju721reb'),
+(1, 'BatteryPower', 11, '2019-06-12 18:03:56', 'ju721reb'),
+(1, 'CPUTemp', 300, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'jitter', 8, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'numOfRTThreads', 0, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'LIDAR', 190, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'Speed', 22, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'BatteryPower', 11, '2019-06-12 18:04:59', 'ju721reb'),
+(1, 'CPUTemp', 300, '2019-06-12 18:05:48', 'rebholju'),
+(1, 'jitter', 8, '2019-06-12 18:05:48', 'rebholju'),
+(1, 'numOfRTThreads', 0, '2019-06-12 18:05:48', 'rebholju'),
+(1, 'LIDAR', 190, '2019-06-12 18:05:48', 'rebholju'),
+(1, 'Speed', 22, '2019-06-12 18:05:48', 'rebholju'),
+(1, 'BatteryPower', 11, '2019-06-12 18:05:48', 'rebholju');
 
 --
 -- Indizes der exportierten Tabellen
