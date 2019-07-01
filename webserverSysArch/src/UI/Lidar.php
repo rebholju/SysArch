8a2c645ed29544cd6646ef0b69682bb013f6876d
Andreas <Andreas@AndreasNotebook>
2019-07-02 01:13:14 +0200

    Lidar updatet

--- webserverSysArch/src/UI/Lidar.php
+++ webserverSysArch/src/UI/Lidar.php
@@ -81,7 +81,7 @@ echo"
 			labels: ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100', '110', '120', '130', '140', '150', '160', '170', '180', '190', '200', '210', '220', '230', '240', '250', '260', '270', '280', '290', '300', '310', '320', '330', '340', '350'],
 			datasets: [{
 				data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37],          
-				label: 'Vehicle 1 Lidar Sensor [y-axis: mm | polar-axis: degree]',
+				label: 'Vehicle 1 Lidar Sensor [radius-axis: mm | degree-axis: grad]',
                 backgroundColor: Chart.helpers.color('#4dc9f6').alpha(0.2).rgbString(),
 				borderColor: '#4dc9f6',
 			}]
